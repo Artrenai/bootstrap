fe93837739c25384ea1194da7ce46a222a65a6dc
Godfrey Davis <gdavis42@cnm.edu>
2017-08-21 23:05:08 -0600

    figuring out how the media object class works

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE   html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, intial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+				integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="css/style.css">
+		<title>Bootstrap</title>
+	</head>
+	<body>
+		<!--bio section using  media class-->
+		<section class="container-fluid">
+			<div class="row bg-dark">
+				<div class="media d-flex align-content-center">
+					<img src="image/biopic.jpg" class="img-fluid col-md-6 ml-0">
+					<div class="media-body text-white ">
+						<div >
+							<h2>Godfrey Davis</h2>
+							Photographer/Retouch Artist/Web Developer
+							<p>Hi! Welcome to my portfolio. It’s nothing too grand, just a place to display all my artwork
+								throughout the years. I’m an Aspiring Engineer. I have been fascinated by the bridge of
+								technology and arts and have been experimenting with the idea since 2005.Learning slowly through
+								different mediums,my ultimate goal is to be a part of the community of content creators who find
+								ways to successfully bridge the benefits of STEM with the Arts and make it useful and functional
+								in the progression of how we interact in society. So thanks for coming to my page. My small
+								steps to a 1,000 mile journey.
+
+							</p>
+						</div>
+					</div>
+				</div>
+		</section>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+				  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+				  crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+				  integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+				  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+				  integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+				  crossorigin="anonymous"></script>
+		<script src="js/script.js"></script>
+	</body>
+</html>
