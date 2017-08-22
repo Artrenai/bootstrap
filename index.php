464ec4c4d7adb4c0ac8a998e601db3c9ecbea24e
Godfrey Davis <gdavis42@cnm.edu>
2017-08-22 10:05:22 -0600

    adding a third party slider. using cdn for gallery in each section

--- index.php
+++ index.php
@@ -7,42 +7,71 @@
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
 				integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.x.x/css/swiper.css">
 
 		<link rel="stylesheet" href="css/style.css">
 		<title>Bootstrap</title>
 	</head>
-	<body>
-		<!--bio section using  media class-->
-		<section class="container-fluid">
-			<div class="row bg-dark">
-				<div class="media d-flex align-content-center">
-					<img src="image/biopic.jpg" class="img-fluid col-md-6 ml-0">
-					<div class="media-body text-white ">
-						<div >
-							<h2>Godfrey Davis</h2>
-							Photographer/Retouch Artist/Web Developer
-							<p>Hi! Welcome to my portfolio. It’s nothing too grand, just a place to display all my artwork
-								throughout the years. I’m an Aspiring Engineer. I have been fascinated by the bridge of
-								technology and arts and have been experimenting with the idea since 2005.Learning slowly through
-								different mediums,my ultimate goal is to be a part of the community of content creators who find
-								ways to successfully bridge the benefits of STEM with the Arts and make it useful and functional
-								in the progression of how we interact in society. So thanks for coming to my page. My small
-								steps to a 1,000 mile journey.
 
-							</p>
-						</div>
-					</div>
+	<body>
+	<!--bio section using  media class-->
+<div class="card-deck">
+	<!---card with biopic background-->
+	<div class="card m-0 col-large-6" id="biopic">
+<div class="container" id="bio">
+</div>
+	</div>
+
+	<!--card with bio description-->
+	<div class="card  bg-dark  text-white m-0">
+		<div class="card-block col-md-12 ">
+			<h2 class="card-header">Godfrey Davis</h2>
+			<div class="container-fluid mx-auto text-wrap" role="dialog">
+				<p class="card-title">Photographer/Retouch Artist/Web Developer</p>
+				<p class="card-text">Hi! Welcome to my portfolio. It’s nothing too grand, just a place to display all my
+					artwork
+					throughout the years. I’m an Aspiring Engineer. I have been fascinated by the bridge of
+					technology and arts and have been experimenting with the idea since 2005.Learning slowly through
+					different mediums,my ultimate goal is to be a part of the community of content creators who find
+					ways to successfully bridge the benefits of STEM with the Arts and make it useful and functional
+					in the progression of how we interact in society. So thanks for coming to my page. My small
+					steps to a 1,000 mile journey.
+				</p>
 				</div>
-		</section>
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-				  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-				  crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
-				  integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
-				  crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
-				  integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
-				  crossorigin="anonymous"></script>
-		<script src="js/script.js"></script>
-	</body>
+			</div>
+		</div>
+</div>
+</div>
+<!--Photography Gallery-->
+	<section class="container-fluid mx-auto text-center" id="title">
+			<div class="card mx-auto text-light text-center" id="title">
+				<div class="card-header ">
+					Photography
+				</div>
+				<div class="card-body">
+					<!--slideshow Gallery-->
+				<div class="swiper-container">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide col-large-4"></div>
+					</div>
+
+				</div>
+				</div>
+			</div>
+	</section>
+</div>
+		<!--script for Plugins-->
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.x.x/js/swiper.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.x.x/js/swiper.jquery.min.js"></script>
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+					  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+					  crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+					  integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+					  crossorigin="anonymous"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+					  integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+					  crossorigin="anonymous"></script>
+			<script src="js/script.js"></script>
+			</body>
 </html>
